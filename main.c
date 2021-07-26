9b90bc789b9fd5d4ade8185be58645b5c0935545
Vladimir Hrechko <grechko_vo@navis.ru>
2021-07-26 14:35:10 +0300

    Little utility for set RTC

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ struct rtc_time {
 #define RTC_RD_TIME   _IOR('p', 0x09, struct rtc_time) /* Read RTC time   */
 #define RTC_SET_TIME  _IOW('p', 0x0a, struct rtc_time) /* Set RTC time    */
 
-static char *rtc_file = "/dev/rtc0";
+static char *rtc_file = "/dev/rtc";
 
 static void print_example(const char *err_msg)
 {
@@ -36,8 +36,7 @@ int main(int argn, char *argv[])
   int counter = 0;
   int rtc_f = 0;
   int ret = 0;
-  struct rtc_time rtc_data, get_data;
-  int result = 0;
+  struct rtc_time rtc_data;
 
 
   if(argn != 2) {
@@ -46,7 +45,6 @@ int main(int argn, char *argv[])
   }
 
   str = argv[1];
-  printf("Input: %s\n", str);
   for(counter = 0; counter < 19; ++counter){
     if(counter == 2 || counter == 5 || counter == 10) {
       if(str[counter] != '/') {
@@ -71,47 +69,20 @@ int main(int argn, char *argv[])
   sscanf(str, "%d/%d/%d/%d:%d:%d", &rtc_data.tm_mday, &rtc_data.tm_mon, &rtc_data.tm_year, 
                                    &rtc_data.tm_hour, &rtc_data.tm_min, &rtc_data.tm_sec);
 
-  if( (rtc_data.tm_year < 2000) && (rtc_data.tm_year < 2000) ){
-    print_example("year should be greater than 1970\n");
+  if( (rtc_data.tm_year < 2000) || (rtc_data.tm_year > 2079) ){
+    print_example("year should be between 2000 and 2079\n");
     return 0;
   }
 
   rtc_data.tm_year = (rtc_data.tm_year - 2000) + 100;
+  rtc_data.tm_mon -= 1;
   rtc_f = open(rtc_file, O_RDONLY);
   if(rtc_f == -1) {
     printf("RTC %s open error\n", rtc_file);
     return 0;
   }
 
-  ret = ioctl(rtc_f, RTC_RD_TIME, &get_data);
-  if(ret == -1) {
-    printf("RTC %s set time error\n", rtc_file);
-    close(rtc_f);
-    return 0;
-  }
-
-  printf("Now: %02d/%02d/%04d/%02d:%02d:%02d\n", get_data.tm_mday, get_data.tm_mon, get_data.tm_year, get_data.tm_hour, get_data.tm_min, get_data.tm_sec);
-  printf("Want to set: %02d/%02d/%04d/%02d:%02d:%02d\n", rtc_data.tm_mday, rtc_data.tm_mon, rtc_data.tm_year, rtc_data.tm_hour, rtc_data.tm_min, rtc_data.tm_sec);
-  result = rtc_data.tm_year - get_data.tm_year;
-
-  if(get_data.tm_year > rtc_data.tm_year)
-    get_data.tm_year -= result;
-  else
-    get_data.tm_year += result;
-
-  printf("Year result: %d\n", result);
-  get_data.tm_year += result;
-  /*get_data.tm_year += 1;
-  if(get_data.tm_mday < 31)
-    get_data.tm_mday += 1;
-  else
-  {
-    get_data.tm_mday = 1;
-    get_data.tm_mon += 1;
-  }*/
-  //get_data.tm_mon += 1;
-  //get_data.tm_mday += 1;
-  ret = ioctl(rtc_f, RTC_SET_TIME, &get_data);
+  ret = ioctl(rtc_f, RTC_SET_TIME, &rtc_data);
   if(ret == -1) {
     printf("RTC %s set time error\n", rtc_file);
     close(rtc_f);
